4670145ca10291483dc87b1665811829f9ce3501
uyriq <105006434+uyriq@users.noreply.github.com>
2022-06-10 16:56:31 +0300

    root_modal_overlay all inline  JSX styles=true moved to appropriate module.css ModalOverlay module do createPortal functionality. Modal module implement    layout, module useModal store state show|hide

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
-import React from 'react';
-import './App.css';
-import useModal from './modal/Usemodal';
-import Modal from './modal/modal';
-import Hello from './component/hello';
-
+import React from "react";
+import "./App.css";
+import useModal from "./modal/Usemodal";
+import Modal from "./modal/modal";
+import Hello from "./component/hello";
 
 function App() {
-  
-  const { isShow: show, toggle: _toggleOpen } = useModal()
+  const { isShow: show, toggle: _toggleOpen } = useModal();
 
   return (
     <div className="App">
-      <h1> Tuto modal </h1>
-      <div className="tuto" >
-        <button
-          className='btn'
-          onClick={_toggleOpen}
-        >
-          Afficher modal
+      <h1> all bout react modal </h1>
+      <div>
+        <button className="btn" onClick={_toggleOpen}>
+          do show modal
         </button>
         <Modal
           isShow={show}
           hide={_toggleOpen}
-          title='modal running'
+          title="modal running"
+          content="hello world!"
         >
           <Hello />
         </Modal>
-     </div>
+      </div>
     </div>
   );
 }
--- src/modal/modal.jsx
+++ src/modal/modal.jsx
@@ -1,98 +1,30 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import PropTypes from 'prop-types';
-import * as AiIcons from 'react-icons/ai';
+import React from "react";
+import ModalOverlay from "./modal-overlay";
+import PropTypes from "prop-types";
+import Styles from "./modal.module.css";
 
-const Modal = ({isShow, hide, title, ...props}) =>
-
-  isShow ?
-    ReactDom.createPortal(
-      <>
-        <div className='modal-overlay'>
-          <div className='modal-wrapper'>
-            <div className='modal-content'>
-              <div className='modal-header'>
-                <h4> {title}</h4>
-                <button
-                  type='button'
-                  className='modal-button-close'
-                  onClick={hide}
-                >
-                 <span> <AiIcons.AiOutlineClose fontSize={30} /></span> 
-                </button>
-              </div>
-              <hr/>
-              <div className='modal-body'>
-                {props.children}
-              </div>
-            </div>
+const Modal = ({ isShow, hide, title, ...props }) =>
+  isShow ? (
+    <ModalOverlay>
+      <div className={Styles.modal_wrapper}>
+        <div className={Styles.modal_content}>
+          <div className={Styles.modal_header}>
+            <h4> {title} </h4>
+            <button
+              type="button"
+              className={Styles.modal_button_close}
+              onClick={hide}
+            >
+              <span> ☒ </span>
+            </button>
           </div>
+          <hr />
+          <div className={Styles.modal_body}>{props.children}</div>
         </div>
+      </div>
+    </ModalOverlay>
+  ) : null;
 
-         <style jsx="true">
-          {`
-            .modal-overlay{
-              position:fixed;
-              top:0%;
-              left:0%;
-              width:100vw;
-              height:100vh;
-              z-index:1040;
-              background-color:rgba(0,0,0,0.5);
-             
-            }
-            .modal-wrapper{
-              position:fixed;
-              top:0;
-              left:0;
-              z-index:1050;
-              width:100%;
-              overflow-x:hidden;
-              overflow-y:auto;
-              height:100%; 
-              outline:0;
-              display:flex;
-              align-items:center;
-            }
-            .modal-content{
-              z-index:100;
-              background-color:#fff;
-              position:relative;
-              margin:auto;
-              border-radius:10px;
-              max-width:550px;
-              width:80%;
-              height:auto;
-              padding:auto;
-              top:-10%;
-              transition:1ms;
-            }
-            .modal-header{
-              display:flex;
-              justify-content:space-between;
-              align-items:center;
-            }
-            .modal-header h4{
-              margin-left:20px;
-            }
-            .modal-button-close{
-              font-size:25px;
-              font-weight:500;
-              color:black;
-              cursor:pointer;
-              border:none;
-             background:transparent;
-            }
-            .modal-body{
-              padding:1rem;
-            }
-            `}
-        </style>
-        
-      </>
-      , document.getElementById('modal')
-    ) : null;
-    
 Modal.propTypes = {
   isShow: PropTypes.bool.isRequired,
   hide: PropTypes.func.isRequired,
